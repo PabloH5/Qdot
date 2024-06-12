77e851698f412ba9934ac010a74c689e81a4b763
Hector-f-Romero <hector.f.romero28@gmail.com>
2024-06-11 19:13:42 -0500

    feat: implement preliminary player customization

---
+++ scripts/custom.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var primary_color:Color = "#49c7a6"
+@export var second_color:Color = "#ec9a22"
+@export var sprite_player:Sprite2D=null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
