8c849dfb01d5f93c85022e23b8a0d9302dc93998
Hector-f-Romero <hector.f.romero28@gmail.com>
2024-06-15 20:29:44 -0500

    feat: implement a new way to change sprite colors

--- scripts/custom.gd
+++ scripts/custom.gd
@@ -1,8 +1,11 @@
+@tool
 extends Node
 
-@export var primary_color:Color = "#49c7a6"
-@export var second_color:Color = "#ec9a22"
-@export var sprite_player:Sprite2D=null
+@onready var primary_color_sprite:Sprite2D = $primary_color_sprite
+@onready var secondary_color_sprite:Sprite2D = $secondary_color_sprite
+
+@export var primary_color:=Color.WHITE : set =_set_primary_color
+@export var secondary_color:Color = "#ffffff" : set =_set_secondary_color
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +15,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _set_primary_color(new_color):
+	if primary_color_sprite:
+		primary_color_sprite.modulate = new_color
+	else:
+		print("PAILA primary color")
+		
+func _set_secondary_color(new_color):
+	if secondary_color_sprite:
+		secondary_color_sprite.modulate = new_color
+	else:
+		print("PAILA secondary color")
 
